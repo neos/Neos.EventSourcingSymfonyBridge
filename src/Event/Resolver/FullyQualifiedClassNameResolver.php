14361d9d96ad0e9dd1e6448527dd04ffdec973f8
Timo Nussbaum <timo.nussbaum@gmx.de>
2021-01-06 11:37:38 +0100

    Task: Moved FullyQualifiedClassNameResolver.

---
+++ src/Event/Resolver/FullyQualifiedClassNameResolver.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neos\EventSourcing\SymfonyBridge\Event\Resolver;
+
+use Neos\EventSourcing\Event\DomainEventInterface;
+use Neos\EventSourcing\Event\EventTypeResolverInterface;
+
+class FullyQualifiedClassNameResolver implements EventTypeResolverInterface
+{
+
+    public function getEventType(DomainEventInterface $event): string
+    {
+        return get_class($event);
+    }
+
+    public function getEventClassNameByType(string $eventType): string
+    {
+        return $eventType;
+    }
+}
